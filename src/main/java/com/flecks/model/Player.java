669f54df40b71b4322752957f41df51495808b7b
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-11 01:43:47 +0530

    Add Player model class

---
+++ src/main/java/com/flecks/model/Player.java
@@ -0,0 +1,16 @@
+package com.flecks.model;
+
+import lombok.Data;
+
+/**
+ * <p>
+ *     This class is basically a Model class for Player.
+ * </p>
+ * @author sofiyan
+ */
+@Data
+public class Player {
+    private String username;
+    private String email;
+    private String password;
+}
