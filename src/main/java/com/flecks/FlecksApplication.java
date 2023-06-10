d2364a4d38e152a08fbca1323a7e00750ce0e3dd
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-11 01:10:38 +0530

    Initialized the Flecks Weather Service

---
+++ src/main/java/com/flecks/FlecksApplication.java
@@ -0,0 +1,13 @@
+package com.flecks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlecksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlecksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flecks/FlecksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flecks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlecksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
