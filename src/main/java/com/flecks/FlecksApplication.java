f308a7ad20f56bd98c629b4ced234ab7fbef27e3
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-13 04:42:36 +0530

    Add restTemplate Bean and update application.yaml

--- src/main/java/com/flecks/FlecksApplication.java
+++ src/main/java/com/flecks/FlecksApplication.java
@@ -1,13 +1,23 @@
 package com.flecks;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class FlecksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlecksApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
